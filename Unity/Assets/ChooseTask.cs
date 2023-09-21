07c84cfa7044bc2c4547cfda87b8aa8cd1a4a239
Daniele Giunchi <dannox@google.com>
2023-09-21 19:17:35 +0100

    add training stage

--- Unity/Assets/ChooseTask.cs
+++ Unity/Assets/ChooseTask.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class ChooseTask : MonoBehaviour
 {
     public GameObject cube;
+    public GameObject cubeT;
+    public GameObject sphereT;
     public GameObject targetArea;
     public GameObject menu;
 
     public GameObject title;
+    public GameObject training;
     public GameObject user;
     public GameObject current;
 
@@ -35,6 +38,7 @@ public class ChooseTask : MonoBehaviour
     public void DisplayTime() 
     {
         menu.SetActive(true);
+        training.SetActive(false);
         user.SetActive(false);
         current.SetActive(false);
         title.GetComponent<UnityEngine.UI.Text>().text = "Time: " + time.ToString("F2");
@@ -45,18 +49,30 @@ public class ChooseTask : MonoBehaviour
         selected = task;
         if (task == 0) 
         {
+            cubeT.SetActive(false);
+            sphereT.SetActive(false);
             cube.GetComponent<MeshRenderer>().enabled = true;
             targetArea.GetComponent<MeshRenderer>().enabled = true;
             targetArea.GetComponent<MeshRenderer>().material = cube.GetComponent<MeshRenderer>().material;
         }
         else if (task == 1) 
         {
+            cubeT.SetActive(false);
+            sphereT.SetActive(false);
             cube.GetComponent<MeshRenderer>().enabled = true;
             targetArea.GetComponent<MeshRenderer>().enabled = true;
         }
         else if (task == 2) 
-        { 
-        
+        {
+            cubeT.SetActive(false);
+            sphereT.SetActive(false);
+            cube.SetActive(false);
+        }
+        else if (task == 3)
+        {
+            cube.SetActive(false);
+            cubeT.GetComponent<MeshRenderer>().enabled = true;
+            sphereT.GetComponent<MeshRenderer>().enabled = true;
         }
         menu.SetActive(false);
     }
