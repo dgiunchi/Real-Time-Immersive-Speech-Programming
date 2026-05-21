50d56efd1aeff417ded3a954e1540152295e3a78
Giacomo Vallasciani <vallasciani.giacomo.94@gmail.com>
2026-05-21 17:13:27 +0100

    unity and ubiq update finished

--- Unity/Assets/RoomJoiner.cs
+++ Unity/Assets/RoomJoiner.cs
@@ -1,21 +1,62 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Collections;
+using Ubiq.Messaging;
 using Ubiq.Rooms;
+using UnityEngine;
 
 public class RoomJoiner : MonoBehaviour
 {
     public string Guid;
+    public float retrySeconds = 1.0f;
+    public bool logJoinAttempts = true;
 
-    // Start is called before the first frame update
-    void Start()
+    private RoomClient roomClient;
+    private NetworkScene networkScene;
+
+    private void Start()
     {
-        GetComponent<RoomClient>().Join(System.Guid.Parse(Guid));
+        roomClient = GetComponent<RoomClient>();
+        networkScene = NetworkScene.Find(this);
+
+        roomClient.OnJoinedRoom.AddListener(room =>
+        {
+            if (logJoinAttempts)
+            {
+                Debug.Log($"[RoomJoiner] joined room name={room.Name} uuid={room.UUID} joincode={room.JoinCode}");
+            }
+        });
+
+        roomClient.OnJoinRejected.AddListener(rejection =>
+        {
+            Debug.LogWarning($"[RoomJoiner] join rejected reason={rejection.reason}");
+        });
+
+        StartCoroutine(JoinWhenConnected());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator JoinWhenConnected()
     {
-        
+        if (!System.Guid.TryParse(Guid, out var roomGuid))
+        {
+            Debug.LogError($"[RoomJoiner] invalid room guid: {Guid}");
+            yield break;
+        }
+
+        while (roomClient && !roomClient.JoinedRoom)
+        {
+            if (networkScene && networkScene.connectionCount > 0)
+            {
+                if (logJoinAttempts)
+                {
+                    Debug.Log($"[RoomJoiner] joining room guid={roomGuid} connections={networkScene.connectionCount}");
+                }
+                roomClient.Join(roomGuid);
+            }
+            else if (logJoinAttempts)
+            {
+                Debug.Log("[RoomJoiner] waiting for server connection before join");
+            }
+
+            yield return new WaitForSeconds(retrySeconds);
+        }
     }
 }
