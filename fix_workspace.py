a146d8a3823283190eea51fa08924a0dc4e0ed50
Sandeep Rai <214435639+sandeeprai-ai@users.noreply.github.com>
2026-08-23 20:47:07 +0100

    chore: remove temporary backend refactoring scripts

--- fix_workspace.py
+++
@@ -1,7 +0,0 @@
-with open("Cargo.toml", "r") as f:
-    lines = f.readlines()
-
-with open("Cargo.toml", "w") as f:
-    for line in lines:
-        if '"apps/sandbox-runner"' not in line:
-            f.write(line)
